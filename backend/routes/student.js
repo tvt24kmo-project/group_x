ead4e758ac30fcda8889a0c52816dd035720576c
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2025-01-07 19:15:35 +0200

    student taulun crud operaatiot tehty ja testattu

---
+++ backend/routes/student.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const user = require('../models/student_model');
+const student = require('../models/student_model');
+
+router.get('/', function(request, response){
+    student.getAll(function(err, result){
+        if(err){
+            response.json(err);
+        }
+        else {
+            response.json(result);
+        }
+    })
+});
+
+router.get('/:id', function(request, response){
+    student.getById(request.params.id,function(err, result){
+        if(err){
+            response.json(err);
+        }
+        else {
+            response.json(result[0]);
+        }
+    })
+});
+
+router.post('/',function(request, response){
+    student.add(request.body, function(err, result){
+        if(err){
+            response.json(err);
+        }
+        else {
+            response.json(result.affectedRows);
+        }
+    })
+});
+
+router.put('/:id',function(request, response){
+    student.update(request.params.id,request.body, function(err, result){
+        if(err){
+            response.json(err);
+        }
+        else {
+            response.json(result.affectedRows);
+        }
+    })
+});
+
+router.delete('/:id',function(request, response){
+    student.delete(request.params.id, function(err,result){
+        if(err){
+            response.json(err);
+        }
+        else {
+            response.json(result.affectedRows);
+        }  
+    })
+});
+
+module.exports=router;
