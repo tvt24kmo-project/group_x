33ae111b7cf12e95420879468d12112259b91c0b
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2025-01-26 10:07:34 +0200

    postman to actions

--- backend/models/student_model.js
+++ backend/models/student_model.js
@@ -3,7 +3,7 @@ const bcrypt=require('bcryptjs');
 
 const student={
     getAll:function(callback) {
-        return db.query('SELECT * FROM student2', callback);
+        return db.query('SELECT * FROM student', callback);
     },
     getById:function(un, callback){
         return db.query('SELECT * FROM student WHERE username=?',[un],callback);
